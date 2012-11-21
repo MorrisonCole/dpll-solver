5c692faaf0878309c5423f3336d8b4bcc21f13c4
Morrison Cole <admin@morrisoncole.co.uk>
2012-11-21 13:56:58 +0000

    Implemented new solver

--- dpll.hs
+++ dpll.hs
@@ -1,13 +1,50 @@
+import Data.Char
 import Data.List
+import Data.Maybe
 
-solveDPLL :: [[Int]] -> Bool
-solveDPLL = foldr (||) False . map solveClause
+getInts :: String -> [Int]
+getInts xs
+    | isValid xs = map read . init $ words xs
+    | otherwise = []
 
-solveClause :: [Int] -> Bool
-solveClause [] = False
-solveClause [x] = True
-solveClause xs = xs == nubBy (\x y -> x == -y) xs
+isValid :: String -> Bool
+isValid [] = False
+isValid [x] = not $ isAlpha x
+isValid (x:xs) = isValid [x] && isValid xs
+
+findUnit :: [[Int]] -> Maybe Int
+findUnit (x:xs)
+    | length x == 1 = Just $ head x
+    | otherwise = Nothing
+
+simplifyUnit :: Int -> [[Int]] -> [[Int]]
+simplifyUnit x (y:ys)
+    | x `elem` y = simplifyUnit x ys -- TODO: This should remove the element only, not the entire list!
+    | -x `elem` y = [] : simplifyUnit x ys -- TODO: Find out what this is actually supposed to do.
+    | otherwise = y : simplifyUnit x ys -- TODO: Find out what this is actually supposed to do.
+
+unitPropagate :: [[Int]] -> [[Int]]
+unitPropagate xs
+    | y == Nothing = xs
+    | otherwise = unitPropagate (simplifyUnit (fromJust y) xs)
+        where y = findUnit xs
+
+containsEmptyClause :: [[Int]] -> Bool
+containsEmptyClause (x:xs)
+    | x == [] = True
+    | otherwise = True && containsEmptyClause xs
+
+pickLiteral :: [[Int]] -> Int
+pickLiteral (x:xs) = head x
+
+dpll :: [[Int]] -> Bool
+dpll xs
+    | containsEmptyClause (unitPropagate (simplifyUnit y xs)) = False 
+    | containsEmptyClause (unitPropagate (simplifyUnit (-y) xs)) = False
+    | otherwise = True
+        where y = pickLiteral xs
 
 main = do
-    let testList = [[4,5,6,-4],[5, -5],[2,-2]]
-    if solveDPLL testList == True then putStrLn "satisfiable" else putStrLn "unsatisfiable"
+    input <- getContents
+    let clauseList = map getInts $ lines input
+    if dpll clauseList == True then putStrLn "satisfiable" else putStrLn "unsatisfiable"
---
+++ test.hs
@@ -0,0 +1,9 @@
+main = do
+    putStrLn "1 2 0" 
+    putStrLn "1 -3 -5 0"
+    putStrLn "-1 2 -4 0" 
+    putStrLn "-1 3 0"
+    --putStrLn "-1 2 0"
+    --putStrLn "-1 -2 3 0"
+    --putStrLn "1 0"
+    --putStrLn "-3 0"

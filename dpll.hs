58df9f1cd1d03b92dd6793585a306de1ee77adcc
Morrison Cole <admin@morrisoncole.co.uk>
2012-11-21 00:18:09 +0000

    Implemented basic DPLL solver

--- dpll.hs
+++ dpll.hs
@@ -1,1 +1,13 @@
-main = putStrLn "Writing to standard output!"
+import Data.List
+
+solveDPLL :: [[Int]] -> Bool
+solveDPLL = foldr (||) False . map solveClause
+
+solveClause :: [Int] -> Bool
+solveClause [] = False
+solveClause [x] = True
+solveClause xs = xs == nubBy (\x y -> x == -y) xs
+
+main = do
+    let testList = [[4,5,6,-4],[5, -5],[2,-2]]
+    if solveDPLL testList == True then putStrLn "satisfiable" else putStrLn "unsatisfiable"
